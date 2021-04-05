986c4deac5f505c9863a241d6a4747da43a0e57f
Sergey Yamnich <sergey.yamnich@gmail.com>
2021-04-06 01:06:23 +0300

    Fix eslint issues

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-const name = readlineSync.question("May I have your name?");
-export { name };
+const name = readlineSync.question('May I have your name?');
+export { name as default };
